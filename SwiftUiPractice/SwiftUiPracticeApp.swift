4452a2e734448defca2efcdf0e10a485380a2854
Cheng-Chong Tang <xxx30091@gmail.com>
2023-05-03 15:17:03 +0800

    Initial Commit

---
+++ SwiftUiPractice/SwiftUiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiPracticeApp.swift
+//  SwiftUiPractice
+//
+//  Created by 唐振中 on 2023/5/3.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
